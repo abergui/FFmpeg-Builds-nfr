1772404fa91f9025f1573eab2e5335e5039c5eef
MartinEesmaa <martin.eesmaa@protonmail.com>
2023-08-02 08:18:44 +1000

    making my own timeout because of slow internet

--- images/base/retry-tool.sh
+++ images/base/retry-tool.sh
@@ -2,12 +2,12 @@
 set -xe
 
 RETRY_COUNTER=0
-MAX_RETRY=10
-CUR_TIMEOUT=120
+MAX_RETRY=50
+CUR_TIMEOUT=2000
 while [[ $RETRY_COUNTER -lt $MAX_RETRY ]]; do
     timeout $CUR_TIMEOUT "$@" && break || sleep $(shuf -i 5-90 -n 1)
     RETRY_COUNTER=$(( $RETRY_COUNTER + 1 ))
-    CUR_TIMEOUT=$(( $CUR_TIMEOUT + 60 ))
+    CUR_TIMEOUT=$(( $CUR_TIMEOUT + 120 ))
     echo "Retry $RETRY_COUNTER..."
 done
 if [[ $RETRY_COUNTER -ge $MAX_RETRY ]]; then
