48928e526c941fb62e7d541be2a486d7da0bcdad
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-01-03 19:17:53 +1100

    Add libilbc script

---
+++ scripts.d/50-ilbc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/TimothyGu/libilbc"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git submodule update --init
+
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" ..
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-libilbc
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libilbc
+}
