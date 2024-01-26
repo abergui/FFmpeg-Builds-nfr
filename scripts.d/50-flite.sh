e747f9af3b1ffbc999ea5156d5e33a1798ca842b
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-01-26 18:11:06 +1100

    Fix flite to apply patch

--- scripts.d/50-flite.sh
+++ scripts.d/50-flite.sh
@@ -6,12 +6,20 @@ ffbuild_enabled() {
     return 0
 }
 
+ffbuild_dockerstage() {
+    to_df "RUN --mount=src=${SELF},dst=/stage.sh --mount=src=patches/flite,dst=/patches run_stage /stage.sh"
+}
+
 ffbuild_dockerbuild() {
+    for patch in /patches/*.patch; do
+        echo "Applying $patch"
+        patch -p1 < "$patch"
+    done
+
     autoreconf -if
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
-        --bindir="$FFBUILD_PREFIX\bin\flite"
         --disable-shared
         --with-pic
     )
