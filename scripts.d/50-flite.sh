ad7c5f0090705612924cd7d5e72ea101bab98a3f
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-01-27 09:53:02 +1100

    Add target command in flite build script
    This may fix the problem for using additional target command.

--- scripts.d/50-flite.sh
+++ scripts.d/50-flite.sh
@@ -33,6 +33,31 @@ ffbuild_dockerbuild() {
         return -1
     fi
 
+    if [[ $TARGET == win64 ]]; then
+        myconf+=(
+            --target=x86_64-win64-gcc
+        )
+        export CROSS="$FFBUILD_CROSS_PREFIX"
+    elif [[ $TARGET == win32 ]]; then
+        myconf+=(
+            --target=x86-win32-gcc
+        )
+        export CROSS="$FFBUILD_CROSS_PREFIX"
+    elif [[ $TARGET == linux64 ]]; then
+        myconf+=(
+            --target=x86_64-linux-gcc
+        )
+        export CROSS="$FFBUILD_CROSS_PREFIX"
+    elif [[ $TARGET == linuxarm64 ]]; then
+        myconf+=(
+            --target=arm64-linux-gcc
+        )
+        export CROSS="$FFBUILD_CROSS_PREFIX"
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
     ./configure "${myconf[@]}"
     make -j$(nproc)
     make install
