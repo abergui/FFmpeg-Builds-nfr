b59b57582f37b7e9b3cb7a9e87997d928e099484
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-04-06 19:51:53 +1100

    Added Google LC3 build script
    This commit can now build using master FFmpeg latest.
    
    Original FFmpeg commit can be found on: https://github.com/FFmpeg/FFmpeg/commit/240fd04db231efa4abad3abbd808140b4112324a

---
+++ scripts.d/50-lc3.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/google/liblc3"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    mkdir build && cd build
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --buildtype=release
+        --default-library=static
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    meson "${myconf[@]}" ..
+    ninja -j$(nproc)
+    ninja install
+}
+
+ffbuild_configure() {
+    echo --enable-liblc3
+}
+
+ffbuild_unconfigure() {
+    echo --disable-liblc3
+}
