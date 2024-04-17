54739bc7d724b7cb3ed2cf46d9303ba22d061147
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-04-18 02:21:25 +1000

    Fix arm64 by adding variable of vvenc & vvdec

--- scripts.d/50-vvdec.sh
+++ scripts.d/50-vvdec.sh
@@ -6,17 +6,19 @@ ffbuild_enabled() {
     return 0
 }
 
+fixarm64=()
+
 ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
     if [[ $TARGET == *arm64 ]]; then
         fixarm64=(
-            -DVVENC_ENABLE_X86_SIMDE=OFF
+            -DVVENC_ENABLE_X86_SIMD=OFF
         )
     fi
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" ..
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" $fixarm64 ..
     make -j$(nproc)
     make install
 }
--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -6,18 +6,20 @@ ffbuild_enabled() {
     return 0
 }
 
+fixarm64=()
+
 ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" $fixarm64 ..
-
-        if [[ $TARGET == *arm64 ]]; then
-        fixarm64=(
-            -DVVENC_ENABLE_X86_SIMDE=OFF
-        )
-        fi
     
+    if [[ $TARGET == *arm64 ]]; then
+    fixarm64=(
+        -DVVENC_ENABLE_X86_SIMD=OFF
+    )
+    fi
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" $fixarm64 ..
     make -j$(nproc)
     make install
 }
