7e9156805cac3ea3e3f4d580ac0d4141ec8e2005
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-06-05 16:58:57 +1000

    Fix vvenc compile error of script

--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -15,7 +15,7 @@ ffbuild_dockerbuild() {
     
     if [[ $TARGET == *arm64 ]]; then
     fixarm64=(
-        -DVVENC_ENABLE_X86_SIMD=OFF
+        -DVVENC_ENABLE_X86_SIMD=OFF -DVVENC_ENABLE_ARM_SIMD=OFF
     )
     fi
 
