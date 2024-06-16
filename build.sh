3dffceea8a32c61f0606aa0da8324d7e1a89fc52
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-06-16 18:52:25 +1000

    Fix configure permission denied

--- build.sh
+++ build.sh
@@ -36,6 +36,7 @@ cat <<EOF >"$BUILD_SCRIPT"
 
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
+    chmod +x configure
 
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
