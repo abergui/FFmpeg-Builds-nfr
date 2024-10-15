f8cbea776e9099753c5b7d63526df6e3aaba2e03
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-10-15 22:36:16 +1100

    Brought back libgsm shell script
    This will work fine after issue is solved. - Martin Eesmaa

---
+++ scripts.d/50-libgsm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/MartinEesmaa/libgsm"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+
+    export CCFLAGS="-DNeedFunctionPrototypes=1 -c -DSASR -DWAV49 -Wno-comment"
+    export INSTALL_ROOT="$FFBUILD_PREFIX"
+    export CC="${FFBUILD_TOOLCHAIN}-gcc"
+
+    make libgsm -j$(nproc)
+    
+    mkdir -p "$FFBUILD_PREFIX/include/gsm"
+    cp lib/libgsm.a "$FFBUILD_PREFIX/lib/"
+    cp include/gsm/*.h "$FFBUILD_PREFIX/include/gsm"
+    cp include/gsm/gsm.h "$FFBUILD_PREFIX/include/"
+}
+
+ffbuild_configure() {
+    echo --enable-libgsm
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libgsm
+}
+
+ffbuild_cflags() {
+    return 0
+}
+
+ffbuild_ldflags() {
+    return 0
+}
