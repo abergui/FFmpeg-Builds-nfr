73f12646eef778ec43a2f07059e6eb1782db1aa5
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-10-21 22:43:29 +1100

    Try add my own pkgconfig of mpeghe
    This will sure to add my own pkg config of ia_mpeghe. - Martin Eesmaa

--- scripts.d/50-mpeghe.sh
+++ scripts.d/50-mpeghe.sh
@@ -17,6 +17,21 @@ ffbuild_dockerbuild() {
     "$RANLIB" libia_mpegh.a
 
     cp libia_mpegh.a "$FFBUILD_PREFIX"/lib
+
+    touch "$FFBUILD_PREFIX"/lib/pkgconfig/ia_mpeghe.pc
+    
+    cat >"$FFBUILD_PREFIX"/lib/pkgconfig/ia_mpeghe.pc <<EOF
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+
+Name: ia_mpeghe
+Description: MPEG-H 3D Audio Low Complexity Profile Encoder.
+
+Requires:
+Libs: -L\${libdir} -lia_mpeghe
+EOF
+
 }
 
 ffbuild_configure() {
