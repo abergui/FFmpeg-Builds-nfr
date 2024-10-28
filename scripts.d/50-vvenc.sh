ebe03cba55575e37c82f42b9b96863e355ef8286
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-10-28 20:31:39 +1100

    Fix syntax error of vvenc shell script

--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -6,25 +6,21 @@ ffbuild_enabled() {
     return 0
 }
 
-fixarm64=()
-
 ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
-    
-    if [[ $TARGET == *arm64 ]]; then
-    fixarm64=(
-        -DVVENC_ENABLE_ARM_SIMD=ON
+    local armsimd=()
+    if [[ $TARGET == *arm* ]]; then
+        armsimd+=( -DVVENC_ENABLE_ARM_SIMD=ON )
 
         if [[ "$CC" != *clang* ]]; then
             export CFLAGS="$CFLAGS -fpermissive -Wno-error=uninitialized -Wno-error=maybe-uninitialized"
             export CXXFLAGS="$CXXFLAGS -fpermissive -Wno-error=uninitialized -Wno-error=maybe-uninitialized"
         fi
-    )
     fi
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DVVENC_ENABLE_LINK_TIME_OPT=OFF -DEXTRALIBS="-lstdc++" "${fixarm64[@]}" ..
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DVVENC_ENABLE_LINK_TIME_OPT=OFF -DEXTRALIBS="-lstdc++" "${armsimd[@]}" ..
     make -j$(nproc)
     make install
 }
