b3d160b366ca7e60add5b76155eb86428931827f
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-10-29 22:55:43 +1100

    Reverting back cause -fpermissive of cc1 in vvenc

--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -6,18 +6,17 @@ ffbuild_enabled() {
     return 0
 }
 
+fixarm64=()
+
 ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
-    local armsimd=()
-    if [[ $TARGET == *arm* ]]; then
-        armsimd+=( -DVVENC_ENABLE_ARM_SIMD=OFF -DVVENC_ENABLE_X86_SIMD=OFF )
-
-        if [[ "$CC" != *clang* ]]; then
-            export CFLAGS="$CFLAGS -fpermissive -Wno-error=uninitialized -Wno-error=maybe-uninitialized"
-            export CXXFLAGS="$CXXFLAGS -fpermissive -Wno-error=uninitialized -Wno-error=maybe-uninitialized"
-        fi
+    if [[ $TARGET == *arm64 ]]; then
+    fixarm64=(
+        -DVVENC_ENABLE_X86_SIMD=OFF 
+        -DVVENC_ENABLE_ARM_SIMD=OFF
+    )
     fi
 
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DVVENC_ENABLE_LINK_TIME_OPT=OFF -DEXTRALIBS="-lstdc++" "${armsimd[@]}" ..
