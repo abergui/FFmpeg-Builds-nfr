3f6bfe70c237478f99597101eda3ebe6341605ee
MartinEesmaa <martin.eesmaa@protonmail.com>
2024-11-04 12:08:49 +1100

    Try force to disable linuxarm64 of uavs3e

--- scripts.d/50-uavs3e.sh
+++ scripts.d/50-uavs3e.sh
@@ -5,7 +5,8 @@ SCRIPT_COMMIT="eca828b2cf05ebb7fef2e2505e3ee2a2156ba2b1"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
-    [[ $TARGET == *arm64 ]] && return -1
+    [[ $TARGET == winarm64 ]] && return -1
+    [[ $TARGET == linuxarm64 ]] && return -1
     return 0
 }
 
